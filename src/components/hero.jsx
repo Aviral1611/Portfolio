a2ed5ba2459e9b7cbe55ca5bda07b74a1e973b22
Aviral <aviral.vbansal16@gmail.com>
2024-10-11 01:03:52 +0530

    Changed profile pic

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants"
-import profilePic from "../assets/aviralProfile.jpg";
+import profilePic from "../assets/aviralProfile1.jpg";
 import {motion} from "framer-motion";
 
 const container = (delay) => ({
